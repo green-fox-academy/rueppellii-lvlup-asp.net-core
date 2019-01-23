aa588ff89e4c6e4038f0e23fee541437e576df63
Vankó <vankoadam85@gmail.com>
2019-01-23 17:39:29 +0100

    Add checking of mediatype and corresponding erroe message

--- rueppellii-lvlup-asp.net-core/Controllers/RestController.cs
+++ rueppellii-lvlup-asp.net-core/Controllers/RestController.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using rueppellii_lvlup_asp.net_core.DTO;
 using rueppellii_lvlup_asp.net_core.Structs;
 
 namespace rueppellii_lvlup_asp.net_core.Controllers
 {
     [Route("admin")]
     [ApiController]
-    public class AdminController : ControllerBase
+    public class AdminController : Controller
     {
         [HttpPost("add")]
         public IActionResult Add()
         {
+            if (Request.ContentType != "application/json")
+            {
+                return StatusCode(415, new ErrorMessage("This server only supports json media type"));
+            }           
             return NotFound(new ErrorMessage("Please provide all fields"));
         }
     }
