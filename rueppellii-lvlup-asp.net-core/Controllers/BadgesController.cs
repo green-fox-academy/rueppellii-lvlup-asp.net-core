48e13c46477a73a074df31cf6e65379d9ed8a806
vmikii <veghmiki@gmail.com>
2019-01-23 16:54:25 +0100

    Create Controllers/BadgesController

---
+++ rueppellii-lvlup-asp.net-core/Controllers/BadgesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace rueppellii_lvlup_asp.net_core.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class BadgesController : Controller
+  {
+  }
+}
