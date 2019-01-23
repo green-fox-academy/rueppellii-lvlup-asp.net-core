afb0fa783bc682592eae984332f0212ea622b8ff
Vankó <vankoadam85@gmail.com>
2019-01-23 19:00:34 +0100

    Add checking of usertokenaut and requestbody values. Levels deserialisation and checking not working.

--- rueppellii-lvlup-asp.net-core/Controllers/RestController.cs
+++ rueppellii-lvlup-asp.net-core/Controllers/RestController.cs
@@ -14,13 +14,23 @@ namespace rueppellii_lvlup_asp.net_core.Controllers
     public class AdminController : Controller
     {
         [HttpPost("add")]
-        public IActionResult Add()
+        public IActionResult Add(AddAdminDTO addAdminDTO)
         {
             if (Request.ContentType != "application/json")
             {
                 return StatusCode(415, new ErrorMessage("This server only supports json media type"));
-            }           
-            return NotFound(new ErrorMessage("Please provide all fields"));
+            }
+            else if (string.IsNullOrEmpty(Request.Headers["usertokenauth"]) ||
+                ((int?)addAdminDTO.Version ?? 0) == 0 ||
+                String.IsNullOrEmpty(addAdminDTO.Name) ||
+                String.IsNullOrEmpty(addAdminDTO.Tag) ||
+                addAdminDTO.Levels == null
+                )
+            {
+                return NotFound(new ErrorMessage("Please provide all fields"));
+            }
+            return StatusCode(201, new ResponseMessage("Success"));
         }
+
     }
 }
