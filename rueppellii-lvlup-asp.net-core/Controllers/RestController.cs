9f24cfd6d8d42a1164d85599dbfc60cb87885d91
Vankó <vankoadam85@gmail.com>
2019-01-23 14:15:41 +0100

    Create Controller folder and RestController

---
+++ rueppellii-lvlup-asp.net-core/Controllers/RestController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace rueppellii_lvlup_asp.net_core.Controllers
+{
+    [ApiController]
+    public class RestController : ControllerBase
+    {
+    }
+}
