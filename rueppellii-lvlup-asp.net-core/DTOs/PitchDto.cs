f4438d216f8eb1d6c6c8bd89584659a00d77eb6a
Mihály Barczi <barczi.mihaly@gmail.com>
2019-01-24 11:48:44 +0100

    Add fields to PitchDto class

--- rueppellii-lvlup-asp.net-core/DTOs/PitchDto.cs
+++ rueppellii-lvlup-asp.net-core/DTOs/PitchDto.cs
@@ -5,7 +5,11 @@ using System.Threading.Tasks;
 
 namespace rueppellii_lvlup_asp.net_core.DTOs
 {
-    public class PitchDTO
+    public class PitchDto
     {
+        public string BadgeName { get; set; }
+        public int OldLevel { get; set; }
+        public int PitchedLvl { get; set; }
+        public string PitchMessage { get; set; }
     }
 }
