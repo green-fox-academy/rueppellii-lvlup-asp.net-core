dc42f555cb4e8c91b389324022f266674f44142b
Mihály Barczi <barczi.mihaly@gmail.com>
2019-01-24 11:17:18 +0100

    Config Startup.cs and add basic PitchController

---
+++ rueppellii-lvlup-asp.net-core/Controllers/PitchController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rueppellii_lvlup_asp.net_core.Controllers
+{
+    [ApiController]
+    public class PitchController : Controller
+    {
+        [HttpPost]
+        [Route("pitch")]
+        public IActionResult Post()
+        {
+            if (string.IsNullOrEmpty(Request.Headers["usertokenauth"]))
+            {
+                return Unauthorized(new KeyValuePair<string, string>("error", "Unauthorized"));
+            }
+            return Ok(Tuple.Create("message", "hello world"));
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Test()
+        {
+            return StatusCode(418, "Hello m8y");
+        }
+    }
+}
