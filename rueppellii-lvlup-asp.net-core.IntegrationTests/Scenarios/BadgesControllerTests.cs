2cf756972135a8d95c6f63eb9040bfaca9de9a81
vmikii <veghmiki@gmail.com>
2019-01-28 17:50:26 +0100

    Add 2 new tests for ListBadges

--- rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/BadgesControllerTests.cs
+++ rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/BadgesControllerTests.cs
@@ -35,9 +35,29 @@ namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios
     public async Task ListBadges_Should_ReturnUnauthorized()
     {
       var request = new HttpRequestMessage(HttpMethod.Get, "/badges");
-      request.Headers.Add("usertokenauth", "");
+      request.Headers.Add("usertokenauth", "invalid");
       var response = await testContext.Client.SendAsync(request);
       Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task ListBadges_Should_ReturnBadges()
+    {
+      string badges = "{ \"badges\": [ { \"name\": \"Process improver\", \"level\": \"2\"}, { \"name\": \"English speaker\", \"level\": \"1\"}, { \"name\": \"Feedback giver\", \"level\": \"1\"} ] }";
+      var request = new HttpRequestMessage(HttpMethod.Get, "/badges");
+      request.Headers.Add("usertokenauth", "gen");
+      var response = await testContext.Client.SendAsync(request);
+      Assert.Equal(badges, response.Content.ReadAsStringAsync().Result);
+    }
+
+    [Fact]
+    public async Task ListBadges_Should_ReturnErrorMessage()
+    {
+      string errorMessage = "{\"error\":\"Unauthorized\"}";
+      var request = new HttpRequestMessage(HttpMethod.Get, "/badges");
+      request.Headers.Add("usertokenauth", "invalid");
+      var response = await testContext.Client.SendAsync(request);
+      Assert.Equal(errorMessage, response.Content.ReadAsStringAsync().Result);
     }
   }
 }
