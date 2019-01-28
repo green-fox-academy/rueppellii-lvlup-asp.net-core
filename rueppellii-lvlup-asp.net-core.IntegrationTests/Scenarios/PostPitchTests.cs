a20e339fe4c62844c034eb920ee28dd63c4ae81f
Mihály Barczi <barczi.mihaly@gmail.com>
2019-01-28 10:58:47 +0100

    Add working CreatePitch returning Created test

--- rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/PostPitchTests.cs
+++ rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/PostPitchTests.cs
@@ -1,7 +1,9 @@
-﻿using rueppellii_lvlup_asp.net_core.Dtos;
+﻿using Newtonsoft.Json;
+using rueppellii_lvlup_asp.net_core.Dtos;
 using rueppellii_lvlup_asp.net_core.IntegrationTests.Fixtures;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -18,11 +20,29 @@ namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios
         }
 
         [Fact]
-        public async Task CreatePitch_Should_Return415()
+        public async Task CreatePitch_Should_ReturnUnsupportedMediaType()
         {
-            var content = new StringContent("asdasdasdasda");
+            var content = new StringContent("Random string text");
             var response = await testContext.Client.PostAsync("/pitch", content);
             Assert.Equal(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreatePitch_Should_ReturnCreated()
+        {
+            var inputDto = new PitchDto()
+            {
+                BadgeName = "English speaker",
+                OldLvl = 2,
+                PitchedLvl = 3,
+                PitchMessage = "Hello World! My English is bloody gorgeous.",
+                Holders = new[] { "balazs.jozsef", "benedek.vamosi", "balazs.barna" }
+            };
+            var json = JsonConvert.SerializeObject(inputDto);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            httpContent.Headers.Add("usertokenauth", "<generated UUID>");
+            var response = await testContext.Client.PostAsync("/pitch", httpContent);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
     }
 }
