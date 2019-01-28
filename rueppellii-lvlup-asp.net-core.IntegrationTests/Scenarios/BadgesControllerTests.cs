2045e1f3d04747dd6ae26271555bb4f44b657a93
vmikii <veghmiki@gmail.com>
2019-01-28 15:35:28 +0100

    Create tests for BadgesController/ListBadges

---
+++ rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/BadgesControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using rueppellii_lvlup_asp.net_core.IntegrationTests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios
+{
+  [Collection("TestCollection")]
+  public class BadgesControllerTests
+  {
+    private readonly TestContext testContext;
+
+    public BadgesControllerTests(TestContext testContext)
+    {
+      this.testContext = testContext;
+    }
+
+    [Fact]
+    public async Task ListBadges_Should_ReturnOK()
+    {
+      var request = new HttpRequestMessage(HttpMethod.Get, "/badges");
+      request.Headers.Add("usertokenauth", "gen");
+      var response = await testContext.Client.SendAsync(request);
+      Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task ListBadges_Should_ReturnUnauthorized()
+    {
+      var request = new HttpRequestMessage(HttpMethod.Get, "/badges");
+      request.Headers.Add("usertokenauth", "");
+      var response = await testContext.Client.SendAsync(request);
+      Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+  }
+}
