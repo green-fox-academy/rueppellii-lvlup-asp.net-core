b845645d3e513921ba498206260612b383d5cece
Vankó <vankoadam85@gmail.com>
2019-01-29 09:07:35 +0100

    Recommit files, that lost tracking

---
+++ rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/AdminControllerTest.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using rueppellii_lvlup_asp.net_core.DTOs;
+using rueppellii_lvlup_asp.net_core.IntegrationTests.Fixtures;
+using rueppellii_lvlup_asp.net_core.IntegrationTests.Mocks;
+using rueppellii_lvlup_asp.net_core.Utility;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios
+{
+    [Collection("TestCollection")]
+    public class AdminControllerTest
+    {
+        private readonly TestContext _testContext;
+
+        public AdminControllerTest(TestContext testContext)
+        {
+            this._testContext = testContext;
+        }
+        [Fact]
+        public async Task Add_Should_Return_Created()
+        {
+            var request = new AddAdminDtoMockHeaders(new AddAdminDtoMockBody().GetCorrectBody()).SetCorrectHeaders();
+            var response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal(JsonConvert.SerializeObject(new ResponseMessage("Success")), await response.Content.ReadAsStringAsync());
+        }
+        [Fact]
+        public async Task Incorrect_ContentType_Should_Return_UnsupportedMediaType()
+        {
+            var request = new AddAdminDtoMockHeaders(new AddAdminDtoMockBody().GetCorrectBody()).SetMissingContentTypeHeader();
+            var response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+
+            response = await _testContext.Client.PostAsync("/admin/add", request.SetXmlContetnType());
+            Assert.Equal(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+        }
+        [Fact]
+        public async Task Missing_Or_Empty_Usertokenauth_Should_Return_Unauthorised()
+        {
+            var request = new AddAdminDtoMockHeaders(new AddAdminDtoMockBody().GetCorrectBody()).SetMissingUsertokenauth();
+            var response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            Assert.Equal(JsonConvert.SerializeObject(new ErrorMessage("usertokenauth missing")), await response.Content.ReadAsStringAsync());
+
+            response = await _testContext.Client.PostAsync("/admin/add", request.SetEmptyUsertokenauth());
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            Assert.Equal(JsonConvert.SerializeObject(new ErrorMessage("usertokenauth missing")), await response.Content.ReadAsStringAsync());
+        }
+        [Fact]
+        public async Task Missing_Or_Empty_RequestBody_Fields_Should_Return_BadRequest()
+        {
+            var request = new AddAdminDtoMockHeaders(new AddAdminDtoMockBody().GetMissingBody()).SetCorrectHeaders();
+            var response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal(JsonConvert.SerializeObject(new ErrorMessage("Please provide all fields")), await response.Content.ReadAsStringAsync());
+
+            request = new AddAdminDtoMockHeaders(new AddAdminDtoMockBody().GetEmptyStringsBody()).SetCorrectHeaders();
+            response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal(JsonConvert.SerializeObject(new ErrorMessage("Please provide all fields")), await response.Content.ReadAsStringAsync());
+        }
+    }
+}
---
+++ rueppellii-lvlup-asp.net-core/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rueppellii_lvlup_asp.net_core.DTOs;
+using rueppellii_lvlup_asp.net_core.Utility;
+
+namespace rueppellii_lvlup_asp.net_core.Controllers
+{
+    [Route("admin")]
+    [ApiController]
+    public class AdminController : Controller
+    {
+        [HttpPost("add")]
+        [Consumes("application/json")]
+        public IActionResult Add(AddAdminDto addAdminDto)
+        {
+            if (string.IsNullOrEmpty(Request.Headers["usertokenauth"]))
+            {
+                return Unauthorized(new ErrorMessage("usertokenauth missing"));
+            }
+            if (((int?)addAdminDto.Version ?? 0) == 0 ||
+                string.IsNullOrEmpty(addAdminDto.Name) ||
+                string.IsNullOrEmpty(addAdminDto.Tag) ||
+                addAdminDto.Levels == null)
+            {
+                return BadRequest(new ErrorMessage("Please provide all fields"));
+            }
+            return StatusCode(201, new ResponseMessage("Success"));
+        }
+    }
+}
