11e49e7d0ce5bad2bca58664b0f942365d5f990b
Vankó <vankoadam85@gmail.com>
2019-02-01 11:24:40 +0100

    Create test for Created return on put pitch endpoint

--- rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/PitchesController/PutPitch.cs
+++ rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/PitchesController/PutPitch.cs
@@ -1,11 +1,15 @@
 ﻿using rueppellii_lvlup_asp.net_core.IntegrationTests.Fixtures;
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios.PitchesController
 {
+    [Collection("TestCollection")]
     public class PutPitch
     {
         private readonly TestContext _testcontext;
@@ -18,7 +22,12 @@ namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios.PitchesContro
         [Fact]
         public async Task Should_ReturnCreated()
         {
-
+            var request = new HttpRequestMessage(HttpMethod.Put, "/pitch");
+            request.Headers.Add("ContentType", "application/json");
+            request.Headers.Add("usertokentauth", "placeholder");
+            var response = await _testcontext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal("{\"message\": \"Success\"}", response.Content.ReadAsStringAsync().Result);
         }
     }
 }
