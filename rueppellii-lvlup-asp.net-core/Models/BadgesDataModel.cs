c2c1381dd339d624ac6d5940969b4ccb0ceb9419
Bea Huszár <husz.beata@gmail.com>
2019-02-01 17:10:13 +0100

    upd: badges column settings

--- rueppellii-lvlup-asp.net-core/Models/BadgesDataModel.cs
+++ rueppellii-lvlup-asp.net-core/Models/BadgesDataModel.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace rueppellii_lvlup_asp.net_core.Models
 {
     public class BadgesDataModel
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(15)]
         public string Version { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Tag { get; set; }
+
+        [Required]
         public List<Levels> Levels { get; set; }
     }
 }
