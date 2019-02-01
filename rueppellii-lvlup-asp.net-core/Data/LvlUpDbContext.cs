b2ecec2c2ac7eb90fc78c5db41ada02fba728216
Bea Huszár <husz.beata@gmail.com>
2019-02-01 16:43:04 +0100

    add: setup for db context

--- rueppellii-lvlup-asp.net-core/Data/LvlUpDbContext.cs
+++ rueppellii-lvlup-asp.net-core/Data/LvlUpDbContext.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace rueppellii_lvlup_asp.net_core.Data
 {
-    public class LvlUpDbContext
+    public class LvlUpDbContext : DbContext
     {
+        public DbSet<BadgesDataModel> Badges { get; set; }
+        public DbSet<UsersDataModel> Users { get; set; }
+        public DbSet<ArchetypesDataModel> Archetypes { get; set; }
+        public DbSet<PitchesDataModel> Pitches { get; set; }
+
+        public LvlUpDbContext(DbContextOptions<LvlUpDbContext> options) : base(options)
+        {
+        }
     }
 }
--- rueppellii-lvlup-asp.net-core/Startup.cs
+++ rueppellii-lvlup-asp.net-core/Startup.cs
@@ -1,19 +1,31 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using rueppellii_lvlup_asp.net_core.Data;
 
 namespace rueppellii_lvlup_asp.net_core
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<LvlUpDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
           services.AddMvc();
         }
             // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
