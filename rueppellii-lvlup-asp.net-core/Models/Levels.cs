94a116fbbcc4b28c41e3565338b07180162d90f3
Bea Huszár <husz.beata@gmail.com>
2019-02-01 16:59:14 +0100

    add: Levels object properties

--- rueppellii-lvlup-asp.net-core/Models/Levels.cs
+++ rueppellii-lvlup-asp.net-core/Models/Levels.cs
@@ -1,6 +1,11 @@
-﻿namespace rueppellii_lvlup_asp.net_core.Models
+﻿using System.Collections.Generic;
+
+namespace rueppellii_lvlup_asp.net_core.Models
 {
     public class Levels
     {
+        public int? Level { get; set; }
+        public string Description { get; set; }
+        public List<string> Holders { get; set; }
     }
 }
