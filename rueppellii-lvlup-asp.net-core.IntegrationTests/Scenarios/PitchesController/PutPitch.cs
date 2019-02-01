86c9437498d4e0a935274cae15d8610541aa86b2
Vankó <vankoadam85@gmail.com>
2019-02-01 10:56:26 +0100

    Create test class for put pitch endpoint

---
+++ rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/PitchesController/PutPitch.cs
@@ -0,0 +1,24 @@
+﻿using rueppellii_lvlup_asp.net_core.IntegrationTests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios.PitchesController
+{
+    public class PutPitch
+    {
+        private readonly TestContext _testcontext;
+
+        public PutPitch(TestContext testcontext)
+        {
+            this._testcontext = testcontext;
+        }
+
+        [Fact]
+        public async Task Should_ReturnCreated()
+        {
+
+        }
+    }
+}
