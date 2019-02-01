5d04ec25e73a8213e68fd0448f738ade7809755f
Vankó <vankoadam85@gmail.com>
2019-02-01 11:59:02 +0100

    Recommit for some untracked files

---
+++ rueppellii-lvlup-asp.net-core.IntegrationTests/Mocks/MockRequest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Mocks
+{
+    public class MockRequest : StringContent
+    {
+        public MockRequest(string content) : base(content)
+        {
+        }
+        public MockRequest SetContentTypeJsonAndUsertokenauth()
+        {
+            this.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            this.Headers.Add("usertokenauth", "<generated UUID>");
+            return this;
+        }
+        public MockRequest SetUsertokenauth()
+        {
+            this.Headers.Add("usertokenauth", "<generated UUID>");
+            return this;
+        }
+        public MockRequest SetContentTypeXmlAndUsertokenauth()
+        {
+            this.Headers.ContentType = new MediaTypeHeaderValue("application/xml");
+            this.Headers.Add("usertokenauth", "<generated UUID>");
+            return this;
+        }
+        public MockRequest SetContentTypeJson()
+        {
+            this.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return this;
+        }
+        public MockRequest SetContentTypeJsonAndEmptyUsertokenauth()
+        {
+            this.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            this.Headers.Add("usertokenauth", string.Empty);
+            return this;
+        }
+    }
+}
---
+++ rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/AdminController/PostAdd.cs
@@ -0,0 +1,66 @@
+﻿using rueppellii_lvlup_asp.net_core.IntegrationTests.Fixtures;
+using rueppellii_lvlup_asp.net_core.IntegrationTests.Mocks;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios.AdminController
+{
+    [Collection("TestCollection")]
+    public class PostAdd
+    {
+        private readonly TestContext _testContext;
+
+        public PostAdd(TestContext testContext)
+        {
+            this._testContext = testContext;
+        }
+
+        [Fact]
+        public async Task Should_ReturnCreated()
+        {
+            var request = new MockRequest(new AddAdminPostRequestMockBody().SetCorrectBody()).SetContentTypeJsonAndUsertokenauth();
+            var response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal("{\"message\":\"Success\"}", response.Content.ReadAsStringAsync().Result);
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnsupportedMediaType()
+        {
+            var request = new MockRequest(new AddAdminPostRequestMockBody().SetCorrectBody()).SetUsertokenauth();
+            var response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+
+            response = await _testContext.Client.PostAsync("/admin/add", request.SetContentTypeXmlAndUsertokenauth());
+            Assert.Equal(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorised()
+        {
+            var request = new MockRequest(new AddAdminPostRequestMockBody().SetCorrectBody()).SetContentTypeJson();
+            var response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            Assert.Equal("{\"error\":\"Unauthorized\"}", response.Content.ReadAsStringAsync().Result);
+
+            response = await _testContext.Client.PostAsync("/admin/add", request.SetContentTypeJsonAndEmptyUsertokenauth());
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            Assert.Equal("{\"error\":\"Unauthorized\"}", response.Content.ReadAsStringAsync().Result);
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest()
+        {
+            var request = new MockRequest(new AddAdminPostRequestMockBody().SetMissingBody()).SetContentTypeJsonAndUsertokenauth();
+            var response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("{\"error\":\"Please provide all fields\"}", response.Content.ReadAsStringAsync().Result);
+
+            request = new MockRequest(new AddAdminPostRequestMockBody().SetEmptyStringsBody()).SetContentTypeJsonAndUsertokenauth();
+            response = await _testContext.Client.PostAsync("/admin/add", request);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("{\"error\":\"Please provide all fields\"}", response.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
