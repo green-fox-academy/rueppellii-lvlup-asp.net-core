d8952d0c1949a5ccbf9b35178e3d620b7a8a92b2
Bea Huszár <husz.beata@gmail.com>
2019-02-01 17:40:29 +0100

    upd: Archetypes column structure

--- rueppellii-lvlup-asp.net-core/Models/Archetypes.cs
+++ rueppellii-lvlup-asp.net-core/Models/Archetypes.cs
@@ -1,6 +1,19 @@
-﻿namespace rueppellii_lvlup_asp.net_core.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace rueppellii_lvlup_asp.net_core.Models
 {
     public class Archetypes
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int? Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string ArchetypeName { get; set; }
+
+        public List<Badges> Badges { get; set; }
     }
 }
