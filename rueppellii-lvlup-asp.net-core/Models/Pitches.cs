7c192a6674b7b93e4fcc964abf3ed99aa8a84c2d
Bea Huszár <husz.beata@gmail.com>
2019-02-03 15:12:23 +0100

    add: pitch model

--- rueppellii-lvlup-asp.net-core/Models/Pitches.cs
+++ rueppellii-lvlup-asp.net-core/Models/Pitches.cs
@@ -1,7 +1,24 @@
-﻿namespace rueppellii_lvlup_asp.net_core.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace rueppellii_lvlup_asp.net_core.Models
 {
     public class Pitches
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Timestamp]
+        public byte[] Timestamp { get; set; }
+
+        public List<Users> Users { get; set; }
+
+        public int OldLevel { get; set; }
+
+        [MaxLength(200)]
+        public string PitchMessage { get; set; }
 
     }
 }
