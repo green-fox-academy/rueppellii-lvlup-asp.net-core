f059926719c0060557ed6c809abe0f9301afdf99
Bea Huszár <husz.beata@gmail.com>
2019-02-03 15:52:44 +0100

    add: reviews model

---
+++ rueppellii-lvlup-asp.net-core/Models/Reviews.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rueppellii_lvlup_asp.net_core.Models
+{
+    public class Review
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
