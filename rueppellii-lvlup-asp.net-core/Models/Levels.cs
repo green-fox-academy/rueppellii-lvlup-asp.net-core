631e48430fa556cf0a29413fa3d56e7164de8b9a
Bea Huszár <husz.beata@gmail.com>
2019-02-03 15:57:05 +0100

    refact: Levels model

--- rueppellii-lvlup-asp.net-core/Models/Levels.cs
+++ rueppellii-lvlup-asp.net-core/Models/Levels.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace rueppellii_lvlup_asp.net_core.Models
 {
     public class Levels
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
         public int? Level { get; set; }
+
+        [MaxLength(500)]
         public string Description { get; set; }
-        public List<string> Holders { get; set; }
+
+        public List<Users> Holders { get; set; }
     }
 }
