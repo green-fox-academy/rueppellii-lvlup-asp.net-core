85988c845654c4b38e250641b4cbefc99c9df6ae
Bea Huszár <husz.beata@gmail.com>
2019-02-05 18:10:17 +0100

    refact: remove redundant annotations

--- rueppellii-lvlup-asp.net-core/Models/UserLevel.cs
+++ rueppellii-lvlup-asp.net-core/Models/UserLevel.cs
@@ -5,15 +5,9 @@ namespace rueppellii_lvlup_asp.net_core.Models
 {
     public class UserLevel
     {
-        [Key]
-        [Column(Order = 1)]
-        [ForeignKey("User")]
         public long UserId { get; set; }
         public User User { get; set; }
 
-        [Key]
-        [Column(Order = 2)]
-        [ForeignKey("Level")]
         public long LevelId { get; set; }
         public Level Level { get; set; }
     }
