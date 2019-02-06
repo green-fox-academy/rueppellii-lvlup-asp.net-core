716e5193d395ea7aa2cae4085fc6f5a01ec09a03
Bea Huszár <husz.beata@gmail.com>
2019-02-06 17:33:51 +0100

    add: application profile for automapper infrastructure setting

---
+++ rueppellii-lvlup-asp.net-core/Utility/ApplicationProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rueppellii_lvlup_asp.net_core.Utility
+{
+    public class ApplicationProfile : AutoMapper.Profile
+    {
+        public ApplicationProfile()
+        {
+
+        }
+    }
+}
