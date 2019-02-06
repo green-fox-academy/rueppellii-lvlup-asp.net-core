7e9f9d599cb8b8e33f4ecbd232a1219d539152df
Bea Huszár <husz.beata@gmail.com>
2019-02-06 21:08:18 +0100

    refact: DTO field naming for std json communication

--- rueppellii-lvlup-asp.net-core/DTOs/PitchDto.cs
+++ rueppellii-lvlup-asp.net-core/DTOs/PitchDto.cs
@@ -8,8 +8,8 @@ namespace rueppellii_lvlup_asp.net_core.Dtos
     public class PitchDto
     {
         public string BadgeName { get; set; }
-        public int? OldLvl { get; set; }
-        public int? PitchedLvl { get; set; }
+        public int? OldLevel { get; set; }
+        public int? PitchedLevel { get; set; }
         public string PitchMessage { get; set; }
         public string[] Holders { get; set; }
     }
