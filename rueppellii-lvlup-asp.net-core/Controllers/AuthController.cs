82e960141d38ef073ce9d09ddf0c42cebaa98bf1
Vankó <vankoadam85@gmail.com>
2019-02-07 11:17:10 +0100

    Add AuthService, including interface and injection to AuthController

--- rueppellii-lvlup-asp.net-core/Controllers/AuthController.cs
+++ rueppellii-lvlup-asp.net-core/Controllers/AuthController.cs
@@ -12,9 +12,17 @@ namespace rueppellii_lvlup_asp.net_core.Controllers
     [Authorize(AuthenticationSchemes = GoogleDefaults.AuthenticationScheme)]
     public class AuthController : Controller
     {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            this._authService = authService;
+        }
+
         [HttpGet("/auth")]
         public IActionResult Auth()
         {
+            var token = _authService.GetToken(User.Claims);
             return Ok("success");
         }
     }
---
+++ rueppellii-lvlup-asp.net-core/Services/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace rueppellii_lvlup_asp.net_core.Services
+{
+    public interface IAuthService
+    {
+        SecurityToken GetTokent(IEnumerable<Claim> claims);
+    }
+}
