57661c862ee8c9d1be88a76816cfb7db8b31050a
Mihály Barczi <barczi.mihaly@gmail.com>
2019-02-11 14:37:55 +0100

    Add BadgeRepository

---
+++ rueppellii-lvlup-asp.net-core/Repositories/BadgeRepository.cs
@@ -0,0 +1,45 @@
+﻿using rueppellii_lvlup_asp.net_core.Data;
+using rueppellii_lvlup_asp.net_core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rueppellii_lvlup_asp.net_core.Repositories
+{
+    public class BadgeRepository : ICrudRepository<Badge>
+    {
+        private readonly LvlUpDbContext context;
+
+        public BadgeRepository(LvlUpDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Badge> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Badge GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Badge entity)
+        {
+            context.Badges.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(Badge entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
