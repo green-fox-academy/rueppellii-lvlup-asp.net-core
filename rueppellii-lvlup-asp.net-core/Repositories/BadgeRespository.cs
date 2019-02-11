8fed7100e53e944754a8c7cb9bb031c91ebed893
Bea Huszár <husz.beata@gmail.com>
2019-02-11 15:37:33 +0100

    basic config

---
+++ rueppellii-lvlup-asp.net-core/Repositories/BadgeRespository.cs
@@ -0,0 +1,44 @@
+﻿using rueppellii_lvlup_asp.net_core.Data;
+using rueppellii_lvlup_asp.net_core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rueppellii_lvlup_asp.net_core.Repositories
+{
+    public class BadgeRespository : ICrudRepository<Badge>
+    {
+        private readonly LvlUpDbContext context;
+
+        public BadgeRespository(LvlUpDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Badge> GetAll()
+        {
+            return context.Badges;
+        }
+
+        public Badge GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Badge entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Badge entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
