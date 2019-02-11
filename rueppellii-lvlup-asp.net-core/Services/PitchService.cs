51da1273fbb54ab0c449fe2a2843c95ebb0f7efc
vmikii <veghmiki@gmail.com>
2019-02-11 17:43:57 +0100

    create PitchService

---
+++ rueppellii-lvlup-asp.net-core/Services/PitchService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using rueppellii_lvlup_asp.net_core.Models;
+using rueppellii_lvlup_asp.net_core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rueppellii_lvlup_asp.net_core.Services
+{
+    public class PitchService
+    {
+        private readonly IMapper mapper;
+        private readonly ICrudRepository<Pitch> pitchRepository;
+
+        public PitchService(ICrudRepository<Pitch> pitchRepository, IMapper mapper)
+        {
+            this.pitchRepository = pitchRepository;
+            this.mapper = mapper;
+        }
+    }
+}
