4a81b180faca9a4f833e67fc7234357af4269255
Vankó <vankoadam85@gmail.com>
2019-02-12 13:51:57 +0100

    Inject UserRepository into AuthService

--- rueppellii-lvlup-asp.net-core/Services/AuthService.cs
+++ rueppellii-lvlup-asp.net-core/Services/AuthService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using rueppellii_lvlup_asp.net_core.Models;
+using rueppellii_lvlup_asp.net_core.Repositories;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -10,16 +12,21 @@ namespace rueppellii_lvlup_asp.net_core.Services
 {
     public class AuthService : IAuthService
     {
-        private IConfiguration Configuration { get; }
+        private readonly IConfiguration _configuration;
+        private readonly ICrudRepository<User> _repository;
 
-        public AuthService(IConfiguration configuration) => this.Configuration = configuration;
+        public AuthService(IConfiguration configuration, ICrudRepository<User> repository)
+        {
+            this._configuration = configuration;
+            this._repository = repository;
+        }
 
         public string GetToken(IEnumerable<Claim> claims)
         {
             var token = new JwtSecurityToken(
                 signingCredentials: this.GetSigningCredentials(this.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256Signature),
-                issuer: Configuration["jwt:issuer"],
-                audience: Configuration["jwt:audience"],
+                issuer: _configuration["jwt:issuer"],
+                audience: _configuration["jwt:audience"],
                 claims: claims,
                 expires: DateTime.UtcNow.AddMinutes(5),
                 notBefore: DateTime.UtcNow);
@@ -27,7 +34,7 @@ namespace rueppellii_lvlup_asp.net_core.Services
         }
 
         private SymmetricSecurityKey GetSymmetricSecurityKey() => 
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["jwt:secretKey"]));
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["jwt:secretKey"]));
 
         private SigningCredentials GetSigningCredentials(SecurityKey key, string algorithm) => 
             new SigningCredentials(key, algorithm);
