723dd18ef792c34e41ae8739fff271d683033a8e
Bea Huszár <husz.beata@gmail.com>
2019-02-13 11:18:31 +0100

    refact: post pitch

--- rueppellii-lvlup-asp.net-core/Services/PitchService.cs
+++ rueppellii-lvlup-asp.net-core/Services/PitchService.cs
@@ -7,19 +7,15 @@ using System.Collections.Generic;
 
 namespace rueppellii_lvlup_asp.net_core.Services
 {
-    public class PostPitchService : ICrudService<PostPitchDto, Pitch>
+    public class PitchService : ICreateService<PostPitchDto>
     {
         private readonly ICrudRepository<Pitch> repository;
         private readonly IMapper mapper;
 
-        public PostPitchService(ICrudRepository<Pitch> repository, IMapper mapper)
+        public PitchService(ICrudRepository<Pitch> repository, IMapper mapper)
         {
             this.repository = repository;
             this.mapper = mapper;
-        }
-        public IEnumerable<PostPitchDto> GetAll()
-        {
-            throw new NotImplementedException();
         }
 
         public void Save(PostPitchDto dto)
