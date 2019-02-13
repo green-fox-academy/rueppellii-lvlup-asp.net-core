66ed8618de95641397a3ff1cb3956e7ed5993a9b
Bea Huszár <husz.beata@gmail.com>
2019-02-13 12:18:40 +0100

    refact: remove redundant usings

--- rueppellii-lvlup-asp.net-core/Repositories/PitchRepository.cs
+++ rueppellii-lvlup-asp.net-core/Repositories/PitchRepository.cs
@@ -1,9 +1,6 @@
 ﻿using rueppellii_lvlup_asp.net_core.Data;
 using rueppellii_lvlup_asp.net_core.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace rueppellii_lvlup_asp.net_core.Repositories
 {
