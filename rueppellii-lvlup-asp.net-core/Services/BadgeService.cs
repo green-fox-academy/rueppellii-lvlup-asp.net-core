4cb21c58bd225371d5650461bc997b3b3dffa1fb
Bea Huszár <husz.beata@gmail.com>
2019-02-14 14:02:39 +0100

    upd: badgeservice implementing crudservice

--- rueppellii-lvlup-asp.net-core/Services/BadgeService.cs
+++ rueppellii-lvlup-asp.net-core/Services/BadgeService.cs
@@ -3,25 +3,32 @@ using AutoMapper;
 using rueppellii_lvlup_asp.net_core.Dtos;
 using rueppellii_lvlup_asp.net_core.Models;
 using rueppellii_lvlup_asp.net_core.Repositories;
+using rueppellii_lvlup_asp.net_core.Services.Interfaces;
 
 namespace rueppellii_lvlup_asp.net_core.Services
 {
-    public class BadgeService : IReadService<BadgeDto, Badge>
+    public class BadgeService : ICrudService<BadgeDto, Badge>
     {
-        private readonly ICrudRepository<Badge> respository;
+        private readonly ICrudRepository<Badge> repository;
         private readonly IMapper mapper;
 
         public BadgeService(ICrudRepository<Badge> respository, IMapper mapper)
         {
-            this.respository = respository;
+            this.repository = respository;
             this.mapper = mapper;
         }
 
         public IEnumerable<BadgeDto> GetAll()
         {
-            var badgeList = respository.GetAll();
+            var badgeList = repository.GetAll();
 
             return mapper.Map<IEnumerable<BadgeDto>>(badgeList);
+        }
+
+        public void Save(BadgeDto dto)
+        {
+            var badgeModel = mapper.Map<Badge>(dto);
+            repository.Save(badgeModel);
         }
     }
 }
