b57b48134401501fde0ee7c8b663bdf62da90437
Vankó <vankoadam85@gmail.com>
2019-02-25 15:00:11 +0100

    GetHeartbeat endpoint tests working properly

--- rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/HeartbeatController/GetHeartbeat.cs
+++ rueppellii-lvlup-asp.net-core.IntegrationTests/Scenarios/HeartbeatController/GetHeartbeat.cs
@@ -1,39 +1,44 @@
-﻿//using rueppellii_lvlup_asp.net_core.IntegrationTests.Fixtures;
-//using System.Net;
-//using System.Net.Http;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using rueppellii_lvlup_asp.net_core.IntegrationTests.Fixtures;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios.HeartbeatController
-//{
-//    [Collection("TestCollection")]
-//    public class GetHeartbeat
-//    {
-//        private readonly TestContext testContext;
+namespace rueppellii_lvlup_asp.net_core.IntegrationTests.Scenarios.HeartbeatController
+{
+    [Collection("TestCollection")]
+    public class GetHeartbeat
+    {
+        private readonly TestContext testContext;
 
-//        public GetHeartbeat(TestContext testContext)
-//        {
-//            this.testContext = testContext;
-//        }
+        public GetHeartbeat(TestContext testContext)
+        {
+            this.testContext = testContext;
+            testContext.Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", testContext.AuthService.GetToken(
+                new[]
+                {
+                    new Claim("test", "test")
+                }));
+        }
 
-//        [Fact]
-//        public async Task Should_ReturnOK()
-//        {
-//            var request = new HttpRequestMessage(HttpMethod.Get, "/heartbeat");
-//            request.Headers.Add("usertokenauth", "OK");
-//            var response = await testContext.Client.SendAsync(request);
-//            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-//            Assert.Equal("{\"message\":\"OK\"}", response.Content.ReadAsStringAsync().Result);
-//        }
+        [Fact]
+        public async Task Should_ReturnOK()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/heartbeat");
+            var response = await testContext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("{\"message\":\"OK\"}", response.Content.ReadAsStringAsync().Result);
+        }
 
-//        [Fact]
-//        public async Task Should_ReturnError()
-//        {
-//            var request = new HttpRequestMessage(HttpMethod.Get, "/heartbeat");
-//            request.Headers.Add("usertokenauth", string.Empty);
-//            var response = await testContext.Client.SendAsync(request);
-//            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
-//            Assert.Equal("{\"error\":\"Unauthorizied\"}", response.Content.ReadAsStringAsync().Result);
-//        }
-//    }
-//}
+        [Fact]
+        public async Task Should_ReturnError()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/heartbeat");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", null);
+            var response = await testContext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+    }
+}
